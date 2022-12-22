3d05333caea17fc927cc849ba06f934516c17344
Aaron Dixon <atdixon@gmail.com>
2022-12-22 12:55:04 -0600

    Now ready for first auto-release

--- src/me/untethr/nostr/version.clj
+++ src/me/untethr/nostr/version.clj
@@ -2,4 +2,4 @@
 
 ;; note: this value is swapped during release so don't change it w/o changing
 ;; release script - should be "0.0.0-release-test" whenever checked-in
-(def version "0.0.0-release-test")
+(def version "SNAPSHOT")
